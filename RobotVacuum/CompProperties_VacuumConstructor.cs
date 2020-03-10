dda0fde49d9ed00c5318235ff32562400fc899d9
Chloride Cull <chloride@devurandom.net>
2020-03-10 18:14:27 +0100

    Version 1.0

---
+++ RobotVacuum/CompProperties_VacuumConstructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RobotVacuum
+{
+    public class CompProperties_VacuumConstructor : CompProperties
+    {
+        public PawnKindDef hatcherPawn;
+
+        public CompProperties_VacuumConstructor()
+        {
+            this.compClass = typeof(CompVacuumConstructor);
+        }
+    }
+}
---
+++ RobotVacuum/CompVacuumConstructor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace RobotVacuum
+{
+    public class CompVacuumConstructor : ThingComp
+
+    {
+        public CompProperties_VacuumConstructor Props => (CompProperties_VacuumConstructor)props;
+
+        public override void CompTick()
+        {
+            var faction = this.parent.Faction ?? Faction.OfPlayer;
+            try
+            {
+                var request = new PawnGenerationRequest(this.Props.hatcherPawn, faction, PawnGenerationContext.NonPlayer, -1, false, true, false, false, true, false, 1f, false, true, true, true, false, false, false, false, 0f, null, 1f, null, null, null, null, null, null, null, null, null, null, null, null);
+                for (int i = 0; i < this.parent.stackCount; i++)
+                {
+                    var pawn = PawnGenerator.GeneratePawn(request);
+                    if (!PawnUtility.TrySpawnHatchedOrBornPawn(pawn, this.parent))
+                    {
+                        Find.WorldPawns.PassToWorld(pawn, PawnDiscardDecideMode.Discard);
+                    }
+                }
+            }
+            finally
+            {
+                this.parent.Destroy(DestroyMode.Vanish);
+            }
+        }
+    }
+}
